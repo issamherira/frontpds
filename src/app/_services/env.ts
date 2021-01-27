0b308fe0a1c8d2bb1ff4cccdd1485a12235ecd89
osmano01 <issam.herira@sesame.com.tn>
2021-01-27 13:19:39 +0100

    first commit

---
+++ src/app/_services/env.ts
@@ -0,0 +1,13 @@
+import { Location } from '@angular/common';
+export class Env {
+    hostname: string;
+    port: string;
+    host:string
+  backUrl: string;
+    constructor() {
+        this.hostname = location.hostname;
+        this.port = "9004"
+
+        this.host=this.hostname+':'+this.port;
+}
+  }
