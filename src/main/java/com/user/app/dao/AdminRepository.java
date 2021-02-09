61b12f992ab77d5623d72b8828f6856aaf08a2a4
osmano01 <issam.herira@sesame.com.tn>
2021-02-09 22:32:28 +0100

    user

---
+++ src/main/java/com/user/app/dao/AdminRepository.java
@@ -0,0 +1,15 @@
+package com.user.app.dao;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.user.app.entites.Admin;
+
+public interface AdminRepository extends MongoRepository<Admin, String> {
+	
+	Admin findOneByRole(String role);
+	List<Admin> findByRole(String role);
+	Admin findByMailAndPassword(String mail,String password);
+
+}
---
+++ src/main/java/com/user/app/entites/Admin.java
@@ -0,0 +1,48 @@
+package com.user.app.entites;
+
+import java.io.Serializable;
+import org.springframework.data.mongodb.core.mapping.Document;
+@Document
+public class Admin extends User implements Serializable {
+	
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+	@org.springframework.data.annotation.Id
+	private String id;
+	private String role;
+	private String address;
+	
+	public Admin() {
+		super();
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public Admin(String firstname, String lastname, String phoneNumber, String birthDate, String mail,
+			String password ,String id, String address) {
+		super(firstname, lastname, phoneNumber, birthDate, mail, password);
+		this.id = id;
+		this.setAddress(address);
+	}
+
+	
+	
+}
